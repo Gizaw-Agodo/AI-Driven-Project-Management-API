cc506905b941d57dfd271b891ca2b6c8375e3c4a
Gizaw <gizawag123@gmail.com>
2025-10-20 09:42:26 +0300

    add-base-file-config

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME : str = "ai-driven-project-management"
     DEBUG: bool = True
-    DATABASE_URL: str = "sqlite+aiosqlite:///./app.db"
 
+    #postgress config 
+    POSTGRESS_USER:str = 'postgress'
+    POSTGRESS_PASSWORD: str = 'postgress'
+    POSTGRESS_HOST : str = 'localhost'
+    POSTGRESS_PORT : str = '5432'
+    POSTGRESS_DB : str "project_management"
 
+    @property
+    def DATABASE_URL():
+        return f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}'
+        
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
