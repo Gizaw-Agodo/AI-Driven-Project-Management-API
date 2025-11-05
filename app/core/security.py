1b93390fb9584efb9872e4c788185037bd40b6f4
Gizaw <gizawag123@gmail.com>
2025-11-05 09:15:07 +0300

    add some security

---
+++ app/core/security.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from typing import Any, Optional
+from passlib.context import CryptContext
+from app.core.config import settings
+from jose import JWTError, jwt
+
+pwd_context = CryptContext( schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12 )
+
+def hash_password(password:str)-> str : 
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password : str) -> bool:
+    try:
+        return pwd_context.verify(plain_password, hashed_password)
+    except:
+        return False
+
+def create_access_token(
+    data : dict[str, Any], 
+    expires_delta : Optional[timedelta] = None
+) -> str : 
+
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    
+    to_encode.update({
+        "exp": expire,
+        "iat": datetime.utcnow(),
+        "type": "access"
+    })
+
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
