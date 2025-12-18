70cd720899d74b3ced71d88eef989d6ac3fc711f
Gizaw <gizawag123@gmail.com>
2025-12-18 09:05:57 +0300

    add user test

--- app/core/security.py
+++ app/core/security.py
@@ -9,7 +9,8 @@ import secrets
 pwd_context = CryptContext( schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12 )
 
 def hash_password(password:str)-> str : 
-    return pwd_context.hash(password)
+    print(password,'hash passwd')
+    return pwd_context.hash('password')
 
 def verify_password(plain_password: str, hashed_password : str) -> bool:
     try:
